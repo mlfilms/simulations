e9e4331d1d59315f9d5d94219b9cf3fc694301a5
Adam Green <adam.green@colorado.edu>
2019-05-30 13:12:07 -0600

    added gridSize and endTime variable to params.py

--- fortran/LandauGin/param.py
+++ fortran/LandauGin/param.py
@@ -5,8 +5,10 @@ kappaL = [i for i in np.linspace(1,1,1)]
 betaL = [i for i in np.linspace(10,10,1)]
 seed = [i for i in np.arange(10)]
 mu = 0
+gridSize = 200
+endTime = 501
 
-params = np.array([(k,beta,mu, 50,501,s) for k in kappaL for beta in betaL for s in seed])
+params = np.array([(k,beta,mu, gridSize,endTime,s) for k in kappaL for beta in betaL for s in seed])
 
 np.savetxt('params.txt',params,delimiter=' ',fmt=['%03.2f','%04.3f','%04.3f','%d','%d','%d'])
 
