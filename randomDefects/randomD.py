dfd6e3fc1eacb8bb567b21f715b882e8b69951ea
Adam Green <adam.green@colorado.edu>
2019-06-04 14:30:21 -0600

    added decrossed option

--- randomDefects/randomD.py
+++ randomDefects/randomD.py
@@ -3,6 +3,16 @@ import matplotlib.pyplot as plt
 import imageio 
 import skimage
 import random
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('decross',nargs='?',help='delta angle of the decrossed polarizers',type = float,default = 0)
+args=parser.parse_args()
+beta = np.pi/2+args.decross/180*np.pi
+def decrossI(beta,image):
+    #beta is the angle between pol and anl (90 for completely crossed)
+    temp= ( np.sin(image)*np.cos(image)*np.sin(beta)-np.sin(image)**2*np.cos(beta)-np.cos(beta))**2
+    return temp/temp.max()
 nDefects = 20
 grid = np.reshape(np.zeros(200*200),(200,200))
 dgrid = np.reshape(np.zeros(200*200),(200,200))
@@ -26,7 +36,7 @@ for i in np.arange(nDefects):
     grid = dGen(grid,dxn,dyn,-1,random.random()*2*np.pi)
     dgrid[dxp,dyp] =1
     dgrid[dxn,dyn] =-1
-imageio.imwrite('training.bmp',skimage.img_as_ubyte(schler(grid)))
+imageio.imwrite('training.bmp',skimage.img_as_ubyte(decrossI(beta, grid)))
 np.savetxt('out.dat',dgrid)
 np.savetxt('defect.dat',dgrid)
 
