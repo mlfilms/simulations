a78aea84a9607941eb3777318bb3ae433bd59af6
Adam Green <adam.green@colorado.edu>
2019-06-04 18:19:23 -0600

    changed randomD.py to allow for uncrossed

--- fortran/MonteCarlo/param.py
+++ fortran/MonteCarlo/param.py
@@ -2,10 +2,10 @@ import numpy as np
 #arguments go like g, beta, N, endT
 
 gL = [i for i in np.linspace(1,1,1)]
-betaL = [i for i in np.linspace(20,20,1)]
+betaL = [i for i in np.linspace(8,8,1)]
 mu = 0
-
-params = np.array([(g,beta,mu, 200,100000) for g in gL for beta in betaL])
+epoch = 501/.05
+params = np.array([(g,beta,mu, 200,epoch) for g in gL for beta in betaL])
 
 np.savetxt('params.txt',params,delimiter=' ',fmt=['%03.2f','%04.3f','%04.3f','%d','%d'])
 
